1e69d5ad71c5b6e112fdee0d7be41ac7454ecd7c
Helllenna <olena.penkova@gmail.com>
2023-03-22 21:49:08 +0200

    2-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+let isTime = localStorage.getItem(KEY);
+
+function CurrentTime(ev) {
+  localStorage.setItem(KEY, ev.seconds);
+}
+
+function getCurrentTime(ev) {
+  if (isTime) {
+    player.setCurrentTime(isTime);
+    player.off('play', getCurrentTime);
+  }
+  player.on('timeupdate', Throttle(CurrentTime, 1000));
+}
+
+player.on('play', getCurrentTime);
