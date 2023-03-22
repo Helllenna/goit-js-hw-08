d8ddc272f213a9eecde9a3ebe88c0a6aca1ced09
Helllenna <olena.penkova@gmail.com>
2023-03-22 21:27:00 +0200

    1-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,4 @@
 import { galleryItems } from './gallery-items';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -7,17 +6,27 @@ let lightbox = new SimpleLightbox('.gallery a', {
   /* options */
 });
 
-const makeElementOfGallery = ({ preview, original, description }) => {
-  return `
-      <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-      </a>
-      `;
-};
+// const makeElementOfGallery = ({ preview, original, description }) => {
+//   return `
+//       <a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>
+//       `;
+// };
 
-const makeElements = galleryItems.map(makeElementOfGallery).join('');
+// const makeElements = galleryItems.map(makeElementOfGallery).join('');
 
-const galleryEl = document.querySelector('.gallery');
-galleryEl.insertAdjacentHTML('afterbegin', makeElements);
+// const galleryEl = document.querySelector('.gallery');
+// galleryEl.insertAdjacentHTML('afterbegin', makeElements);
 
 new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+const galleryEl = document.querySelector('.gallery');
+const addGalleryItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+  )
+  .join('');
+galleryEl.insertAdjacentHTML('afterbegin', addGalleryItems);
