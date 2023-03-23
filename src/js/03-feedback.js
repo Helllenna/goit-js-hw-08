e78d125c9d1a2df3a87a0a9eb9d4e6b52aa693d8
Helllenna <olena.penkova@gmail.com>
2023-03-23 13:21:45 +0200

    3-2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,18 +5,18 @@ const KEY = 'feedback-form-state';
 
 
 function saveFormState() {
-  const email = document.querySelector('[name="email"]').value;
-  const message = document.querySelector('[name="message"]').value;
-  const data = { email, message };
+  const emailEl = document.querySelector('[name="email"]').value;
+  const messageEl = document.querySelector('[name="message"]').value;
+  const data = { emailEl, messageEl };
   localStorage.setItem(KEY, JSON.stringify(data));
 }
 
 function loadFormState() {
   const data = localStorage.getItem(KEY);
   if (data) {
-    const { email, message } = JSON.parse(data);
-    document.querySelector('[name="email"]').value = email;
-    document.querySelector('[name="message"]').value = message;
+    const { emailEl, messageEl } = JSON.parse(data);
+    document.querySelector('[name="email"]').value = emailEl;
+    document.querySelector('[name="message"]').value = messageEl;
   }
 }
 
