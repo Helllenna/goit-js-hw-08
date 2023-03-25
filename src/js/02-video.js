46081e98bd040f7002f57ca279d5200212475f2c
Helllenna <olena.penkova@gmail.com>
2023-03-25 21:55:34 +0200

    2-6

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,16 +6,15 @@ const player = new Player(iframe);
 const KEY = 'videoplayer-current-time';
 let isTime = localStorage.getItem(KEY);
 
-
 player.on('timeupdate', throttle(function (event) {
   localStorage.setItem(KEY, JSON.stringify(event));
 }, 1000));
 
-function currentTimeOfPlayer() {
+function loadCurrentTime() {
   const time = JSON.parse(isTime);
 
   if (time) {
     player.setCurrentTime(time.seconds);
   }
 }
-currentTimeOfPlayer();
+loadCurrentTime();
